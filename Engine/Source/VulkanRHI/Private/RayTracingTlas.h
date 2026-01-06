5e59b28c3700bb6e4d7ef7fc90d7b49490a5bb92
Neil Mewada <neelrmewada@gmail.com>
2026-01-05 22:44:08 -0800

    sync

--- Engine/Source/VulkanRHI/Private/RayTracingTlas.h
+++ Engine/Source/VulkanRHI/Private/RayTracingTlas.h
@@ -2,5 +2,31 @@
 
 namespace CE::Vulkan
 {
+
+    class RayTracingTlas : public RHI::RayTracingTlas
+    {
+        CE_NO_COPY(RayTracingTlas)
+    public:
+
+        RayTracingTlas(VulkanDevice* device, const RHI::RayTracingTlasDescriptor& desc);
+
+		~RayTracingTlas() override;
+
+    private:
+
+        VulkanDevice* device = nullptr;
+
+		Vulkan::Buffer* tlasBuffer = nullptr;
+        Vulkan::Buffer* scratchBuffer = nullptr;
+        RayTracingAccelerationStructure* accelerationStructure = nullptr;
+        Array<VkAccelerationStructureInstanceKHR> vkInstances;
+        Vulkan::Buffer* tlasInstancesBuffer = nullptr;
+
+        VkAccelerationStructureBuildRangeInfoKHR offsetInfo{};
+		VkAccelerationStructureBuildGeometryInfoKHR buildInfo{};
+        u32 instanceCount = 0;
+
+		friend class CommandList;
+    };
     
 } // namespace CE::Vulkan
--- Engine/Source/VulkanRHI/Private/VulkanRHIPrivate.h
+++ Engine/Source/VulkanRHI/Private/VulkanRHIPrivate.h
@@ -45,6 +45,7 @@
 // Ray Tracing
 #include "RayTracingAccelerationStructure.h"
 #include "RayTracingBlas.h"
+#include "RayTracingTlas.h"
 
 // Frame
 #include "Scope.h"
